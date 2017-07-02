bb82cf5db2843b2be98d2d5c4b79e5c5a7975620
sai <saisondarkar18@gmail.com>
2017-07-02 23:14:55 +0530

    Unused code removed

--- app/src/main/java/in/techmafiya/neversettlewallpaper/Activities/MainActivity.java
+++ app/src/main/java/in/techmafiya/neversettlewallpaper/Activities/MainActivity.java
@@ -100,12 +100,6 @@ public class MainActivity extends AppCompatActivity implements ImagesAdapter.Ima
         setContentView(R.layout.activity_main);
         Paper.init(this);
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            Window w = getWindow();
-//            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-//            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-//        }
-
         initMarshmallowPermission();
 
         initUiElements();
@@ -126,19 +120,6 @@ public class MainActivity extends AppCompatActivity implements ImagesAdapter.Ima
 
 
     }
-
-    public void FullScreencall() {
-        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-            View v = this.getWindow().getDecorView();
-            v.setSystemUiVisibility(View.GONE);
-        } else if (Build.VERSION.SDK_INT >= 19) {
-            //for new api versions.
-            View decorView = getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-            decorView.setSystemUiVisibility(uiOptions);
-        }
-    }
-
 
 
     void initUiElements() {
@@ -212,7 +193,6 @@ public class MainActivity extends AppCompatActivity implements ImagesAdapter.Ima
 
         GetDisplaySize();
         placeholderImage.setImageDrawable(previewImage);
-
         Glide.with(MainActivity.this).
                 load(wallpaperList.get(position).getF())
                 .asBitmap()
@@ -557,59 +537,6 @@ public class MainActivity extends AppCompatActivity implements ImagesAdapter.Ima
 
     }
 
-
-    public static interface ClickListener {
-        public void onClick(View view, int position);
-
-        public void onLongClick(View view, int position);
-    }
-
-
-}
-
-class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
-
-    private MainActivity.ClickListener clicklistener;
-    private GestureDetector gestureDetector;
-
-    public RecyclerTouchListener(Context context, final RecyclerView recycleView, final MainActivity.ClickListener clicklistener) {
-
-        this.clicklistener = clicklistener;
-        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return true;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent e) {
-                View child = recycleView.findChildViewUnder(e.getX(), e.getY());
-                if (child != null && clicklistener != null) {
-                    clicklistener.onLongClick(child, recycleView.getChildAdapterPosition(child));
-                }
-            }
-        });
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-        View child = rv.findChildViewUnder(e.getX(), e.getY());
-        if (child != null && clicklistener != null && gestureDetector.onTouchEvent(e)) {
-            clicklistener.onClick(child, rv.getChildAdapterPosition(child));
-        }
-
-        return false;
-    }
-
-    @Override
-    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-
-    }
-
-    @Override
-    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-    }
 }
 
 class SpacesItemDecoration extends RecyclerView.ItemDecoration {
